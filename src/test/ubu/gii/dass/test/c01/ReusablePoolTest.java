87bcd933e2e81d18c6c834f362dfa3038072ba35
P0NLEE <71222275+P0NLEE@users.noreply.github.com>
2023-02-26 20:25:46 +0100

    GetInstance

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -42,7 +42,7 @@ public class ReusablePoolTest {
 	@Test
 	public void testGetInstance() {
 		if (reusablePool == null) {
-			assert ReusablePool.getInstance() != null : "Debe devolverse objetos en caso de que el Pool sea nulo";
+			assertTrue(ReusablePool.getInstance() instanceof ReusablePool);
 		} else {
 			assertTrue(ReusablePool.getInstance() instanceof ReusablePool);
 		}
